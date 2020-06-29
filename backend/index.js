49723b125a21bdb9417e08c591a4424186a27089
Albert Gozzi <albertgozzi@gmail.com>
2020-06-28 22:31:36 -0400

    changed path for build

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ const server = http.Server(app);
 app.use(cors());
 app.use(express.static(path.join(__dirname, '../../client/build')));
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client/build/', 'index.html'));
+  res.sendFile(path.join(__dirname, '../client/build/', 'index.html'));
 });
 
 // Starting server
